7a76482f3f35897f595e03d4241b3f815a74f8f4
Danier Martinez <dmartinez@wawand.co>
2023-01-30 18:00:03 -0500

    step 6: using css modules to style and Layout shared component between pages

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,25 +1,28 @@
 import Link from "next/link";
 import Head from "next/head";
 import Script from "next/script";
+import Layout from "../../components/layout";
 
 export default function Post() {
     return (
         <>
-            <Head>
-                <title>My first post</title>
-            </Head>
-            <Script
-                src='https://connect.facebook.net/en_US/sdk.js'
-                strategy='lazyOnload'
-                onLoad={() => console.log(`script loaded correctly, window.FB has been populated`)}
-            >
+            <Layout>
+                <Head>
+                    <title>My first post</title>
+                </Head>
+                <Script
+                    src='https://connect.facebook.net/en_US/sdk.js'
+                    strategy='lazyOnload'
+                    onLoad={() => console.log(`script loaded correctly, window.FB has been populated`)}
+                >
 
-            </Script>
+                </Script>
 
-            <h1>My first post</h1>
-            <h2>
-               <Link href='/'>Back to home</Link>
-            </h2>
+                <h1>My first post</h1>
+                <h2>
+                   <Link href='/'>Back to home</Link>
+                </h2>
+            </Layout>
         </>
     )
 }
