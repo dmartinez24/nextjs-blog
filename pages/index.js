9c1589736f9440f9f84cbc7a437eca1275c94d05
Danier Martinez <dmartinez@wawand.co>
2023-02-10 15:58:33 -0500

    minor change

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
           <title>{siteTitle}</title>
         </Head>
         <section className={utilStyles.headingMd}>
-          <p>Hello I'm Danier! I like Genshin Impact a lot :D</p>
+          <p>Hello I'm Danier! I like Mobile Legends a lot :D</p>
           <p>
             (This is a sample website - you’ll be building a site like this on{' '}
             <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
